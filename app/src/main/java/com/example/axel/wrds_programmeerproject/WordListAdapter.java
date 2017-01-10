efa17e1546d14c19ba5e251104645586c31d53c9
MangoDream1 <axel.verheul@gmail.com>
2017-01-10 12:35:18 +0100

    Started WordListAdapter

--- app/src/main/java/com/example/axel/wrds_programmeerproject/WordListAdapter.java
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/WordListAdapter.java
@@ -3,6 +3,7 @@ package com.example.axel.wrds_programmeerproject;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by axel on 10-1-17.
@@ -11,8 +12,25 @@ import android.view.ViewGroup;
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
-            super(itemView);
+        private TextView title;
+        private TextView nWords;
+        private TextView desc;
+
+        private TextView date;
+        private TextView creator;
+        private TextView language;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            title = (TextView) v.findViewById(R.id.title_textView);
+            nWords = (TextView) v.findViewById(R.id.nWords_textView);
+            desc = (TextView) v.findViewById(R.id.desc_textView);
+
+            date = (TextView) v.findViewById(R.id.date_textView);
+            creator = (TextView) v.findViewById(R.id.creator_textView);
+            language = (TextView) v.findViewById(R.id.language_textView);
+
         }
     }
 

262e2c11287012ee055897112744f798fb50e447
MangoDream1 <axel.verheul@gmail.com>
2017-01-10 14:50:25 +0100

    Started DatabaseManager

--- app/src/main/java/com/example/axel/wrds_programmeerproject/DatabaseManager.java
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/DatabaseManager.java
@@ -1,8 +1,41 @@
 package com.example.axel.wrds_programmeerproject;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.SQLException;
+
 /**
  * Created by axel on 10-1-17.
  */
 
 public class DatabaseManager {
+    private static DatabaseManager instance = null;
+
+    private static DatabaseHelper dbHelper;
+    private static SQLiteDatabase database;
+
+    private DatabaseManager() {
+        // Only exists to defeat instantiation
+    }
+
+    public static DatabaseManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+
+        return instance;
+    }
+
+    protected void open(Context context) throws SQLException {
+        // Open database from context
+        dbHelper = new DatabaseHelper(context);
+        database = dbHelper.getWritableDatabase();
+    }
+
+    protected void close() {
+        dbHelper.close();
+    }
+
+
 }

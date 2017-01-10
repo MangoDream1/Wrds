7a2045834410c386a15dc9e733443f21074f83e3
MangoDream1 <axel.verheul@gmail.com>
2017-01-10 13:47:01 +0100

    Finished initial DatabaseHelper with the start layout of the database

---
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/DatabaseHelper.java
@@ -0,0 +1,67 @@
+package com.example.axel.wrds_programmeerproject;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by axel on 10-1-17.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+    static final String DB_NAME = "wrds.db";
+    static final int DB_VERSION = 0;
+
+    // Table names
+    public static final String listTable = "ListTable";
+    public static final String wordTable = "WordTable";
+
+    // List table columns
+    public static final String pk_list_id = "list_id";
+    public static final String str_title = "title";
+    public static final String str_desc = "desc";
+    public static final String dt_createdAt = "createdAt";
+    public static final String str_creator = "creator";
+    public static final String str_languageA = "languageA";
+    public static final String str_languageB = "languageB";
+
+    // Word table columns
+    public static final String pk_word_id = "word_id";
+    public static final String fk_list_id = "list_id";
+    public static final String str_word = "word";
+    public static final String str_translation = "translation";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // Create database queries
+    public static final String createListTable = "create timetable " + listTable + "(" +
+            pk_list_id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            str_title + " TEXT NOT NULL, " +
+            str_desc + " TEXT, " +
+            dt_createdAt + " DATETIME DEFAULT CURRENT_DATE, " +
+            str_creator + " TEXT NOT NULL, " +
+            str_languageA + " TEXT NOT NULL, " +
+            str_languageB + " TEXT NOT NULL);";
+
+    public static final String createWordTable = "create timetable " + wordTable + "(" +
+            pk_word_id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            fk_list_id + "INTEGER NOT NULL, " +
+            str_word + "TEXT NOT NULL, " +
+            str_translation + "TEXT NOT NULL);";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createListTable);
+        db.execSQL(createWordTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + listTable);
+        db.execSQL("DROP TABLE IF EXISTS " + wordTable);
+        onCreate(db);
+    }
+}

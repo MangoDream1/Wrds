6588ee00028fe66825abc71835c6b7d126666897
MangoDream1 <axel.verheul@gmail.com>
2017-01-11 17:37:29 +0100

    Basic adding of lists.

---
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/CMDialog.java
@@ -0,0 +1,71 @@
+package com.example.axel.wrds_programmeerproject;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by axel on 11-1-17.
+ */
+
+public class CMDialog extends DialogFragment {
+
+    // TODO split modify and create from eachother based on dialog creation location
+
+    private DatabaseManager dbm;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Activity activity = getActivity();
+
+        // Use the Builder class for convenient dialog construction
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        // Get the layout inflater
+        LayoutInflater inflater = activity.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        final View view = inflater.inflate(R.layout.cm_dialog, null);
+
+        dbm = DatabaseManager.getInstance();
+
+        builder.setView(view);
+
+        builder.setMessage("Modify/Create list")
+                .setPositiveButton("Modify/Create", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // TODO also do modify here not only create
+
+                        String name = ((EditText) view.findViewById(R.id.list_name_editText))
+                                .getText().toString();
+                        String desc = ((EditText) view.findViewById(R.id.list_description_editText))
+                                .getText().toString();
+                        String lanA = ((EditText) view.findViewById(R.id.list_languageA_editText))
+                                .getText().toString();
+                        String lanB = ((EditText) view.findViewById(R.id.list_languageB_editText))
+                                .getText().toString();
+
+                        dbm.insertList(name, desc, "you", lanA, lanB);
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // User cancelled the dialog
+                        // Empty since this does nothing except close
+                    }
+                });
+
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/axel/wrds_programmeerproject/MainActivity.java
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/MainActivity.java
@@ -1,19 +1,43 @@
 package com.example.axel.wrds_programmeerproject;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            DatabaseManager.getInstance().open(this);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         // Creates toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_menu);
         setSupportActionBar(myToolbar);
 
+        Button add_button = (Button) findViewById(R.id.add_button);
+        add_button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_button:
+                CMDialog cmDialog = new CMDialog();
+                cmDialog.show(getFragmentManager(), "CMDialog");
+        }
     }
 }
+

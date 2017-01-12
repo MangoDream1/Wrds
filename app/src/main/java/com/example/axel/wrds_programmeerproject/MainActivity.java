dc3cf4ca1cf27a6fdf5a3587023a8a12816a6d47
MangoDream1 <axel.verheul@gmail.com>
2017-01-12 10:49:17 +0100

    Added ListActivity. Can also reach it

--- app/src/main/java/com/example/axel/wrds_programmeerproject/MainActivity.java
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.axel.wrds_programmeerproject;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.sql.SQLException;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
 
     private DatabaseManager dbm = DatabaseManager.getInstance();
     private WordListsCursorAdapter adapter;
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
         // TODO set empty listView.setEmptyView()
+        listView.setOnItemClickListener(this);
 
     }
 
@@ -51,6 +57,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 CMDialog cmDialog = new CMDialog();
                 cmDialog.show(getFragmentManager(), "CMDialog");
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("Test id:", String.valueOf(id));
+        Log.d("Test pos:", String.valueOf(position));
+
+        Context context = view.getContext();
+        Intent intent = new Intent(context, ListActivity.class);
+        intent.putExtra("id", id);
+
+        context.startActivity(intent);
+
     }
 
     public void dataChange() {

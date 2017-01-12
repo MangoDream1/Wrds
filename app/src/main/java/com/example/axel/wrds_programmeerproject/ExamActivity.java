9cdf2162f3b1ef43e10a0d0eb3b265c7f7763e98
MangoDream1 <axel.verheul@gmail.com>
2017-01-12 14:41:59 +0100

    Started ExamActivity xml

--- app/src/main/java/com/example/axel/wrds_programmeerproject/ExamActivity.java
+++ app/src/main/java/com/example/axel/wrds_programmeerproject/ExamActivity.java
@@ -1,8 +1,13 @@
 package com.example.axel.wrds_programmeerproject;
 
-/**
- * Created by axel on 12-1-17.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class ExamActivity {
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
 }

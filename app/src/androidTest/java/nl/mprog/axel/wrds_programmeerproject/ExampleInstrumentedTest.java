844251054874535842889fb56cff4332d7f9656c
MangoDream1 <axel.verheul@gmail.com>
2017-01-15 17:13:51 +0100

    Correctly changed name of project to nl.mprog

--- app/src/androidTest/java/nl/mprog/axel/wrds_programmeerproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nl/mprog/axel/wrds_programmeerproject/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.axel.wrds_programmeerproject", appContext.getPackageName());
+        assertEquals("nl.mprog.axel.wrds_programmeerproject", appContext.getPackageName());
     }
 }

c6491ddcde4663c84e818f89cb94f83c7cc5c4fe
MangoDream1 <axel.verheul@gmail.com>
2017-01-16 17:06:08 +0100

    Simple ResultActivity is finished now shows a score

--- app/src/main/java/nl/mprog/axel/wrds_programmeerproject/ResultActivity.java
+++ app/src/main/java/nl/mprog/axel/wrds_programmeerproject/ResultActivity.java
@@ -1,7 +1,10 @@
 package nl.mprog.axel.wrds_programmeerproject;
 
+import android.content.Intent;
+import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        int nMistakes = intent.getIntExtra("nMistakes", 0);
+        int sizeList = intent.getIntExtra("sizeList", 0);
+
+        TextView score = (TextView) findViewById(R.id.score);
+        score.setText(calculateScore(sizeList, nMistakes));
+
     }
+
+    private String calculateScore(int total, int nMistakes) {
+        if (nMistakes > total) {
+            return "1";
+        }
+
+        return String.valueOf(Math.round(100 + 900 / ((double) total / nMistakes))/100);
+    }
+
 }
+

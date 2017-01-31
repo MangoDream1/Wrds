9e7ade513370f79e58f818d75787563bf2f9f413
MangoDream1 <axel.verheul@gmail.com>
2017-01-31 12:24:47 +0100

    Added comments to Adapters

--- app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Adapters/WordListsCursorAdapter.java
+++ app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Adapters/WordListsCursorAdapter.java
@@ -16,9 +16,19 @@ import nl.mprog.axel.wrds_programmeerproject.R;
 
 /**
  * Created by axel on 11-01-17.
+ *
+ * Adapter that fills ListView in MainActivity with information of a list.
+ *
  */
 
 public class WordListsCursorAdapter extends ResourceCursorAdapter {
+
+    private TextView title;
+    private TextView nWords ;
+    private TextView desc;
+    private TextView date;
+    private TextView creator;
+    private TextView language;
 
     public WordListsCursorAdapter(Context context, int layout, Cursor c, int flags) {
         super(context, layout, c, flags);
@@ -29,15 +39,38 @@ public class WordListsCursorAdapter extends ResourceCursorAdapter {
         long id = cursor.getLong(cursor.getColumnIndex(DatabaseHelper.PK_LIST_ID));
         ArrayList<Long> selectedItemsList = ((MainActivity) context).getSelectedItemsList();
 
+        findViews(v);
+        setView(cursor, id);
+
+        // If id in selectedItemsList than is selected thus color gray
+        if (selectedItemsList.contains(id)) {
+            v.setBackgroundColor(Color.LTGRAY);
+        } else {
+            v.setBackgroundColor(Color.TRANSPARENT);
+        }
+    }
+
+    /**
+     * Find the views
+     * @param v view
+     */
+    private void findViews(View v) {
         // Finds views
-        TextView title = (TextView) v.findViewById(R.id.title_textView);
-        TextView nWords = (TextView) v.findViewById(R.id.nWords_textView);
-        TextView desc = (TextView) v.findViewById(R.id.desc_textView);
+        title = (TextView) v.findViewById(R.id.title_textView);
+        nWords = (TextView) v.findViewById(R.id.nWords_textView);
+        desc = (TextView) v.findViewById(R.id.desc_textView);
 
-        TextView date = (TextView) v.findViewById(R.id.date_textView);
-        TextView creator = (TextView) v.findViewById(R.id.creator_textView);
-        TextView language = (TextView) v.findViewById(R.id.language_textView);
+        date = (TextView) v.findViewById(R.id.date_textView);
+        creator = (TextView) v.findViewById(R.id.creator_textView);
+        language = (TextView) v.findViewById(R.id.language_textView);
+    }
 
+    /**
+     * Set the data in the view
+     * @param cursor database cursor
+     * @param id list id
+     */
+    private void setView(Cursor cursor, long id) {
         // Set views
         title.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_TITLE)));
         desc.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_DESC)));
@@ -55,10 +88,5 @@ public class WordListsCursorAdapter extends ResourceCursorAdapter {
         nWords.setText(" (" + String.valueOf(DatabaseManager.getInstance()
                 .countListWords(id)) + ")");
 
-        if (selectedItemsList.contains(id)) {
-            v.setBackgroundColor(Color.LTGRAY);
-        } else {
-            v.setBackgroundColor(Color.TRANSPARENT);
-        }
     }
 }
--- app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Adapters/WordsCursorAdapter.java
+++ app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Adapters/WordsCursorAdapter.java
@@ -15,9 +15,15 @@ import nl.mprog.axel.wrds_programmeerproject.R;
 
 /**
  * Created by axel on 12-1-17.
+ *
+ * Adapter that fills ListView in ListActivity with words
  */
 
 public class WordsCursorAdapter extends ResourceCursorAdapter {
+
+    private TextView index;
+    private TextView wordA;
+    private TextView wordB;
 
     public WordsCursorAdapter(Context context, int layout, Cursor c, int flags) {
         super(context, layout, c, flags);
@@ -28,18 +34,36 @@ public class WordsCursorAdapter extends ResourceCursorAdapter {
         long id = cursor.getLong(cursor.getColumnIndex(DatabaseHelper.PK_LIST_ID));
         ArrayList<Long> selectedItemsList = ((ListActivity) context).getSelectedItemsList();
 
-        TextView index = (TextView) v.findViewById(R.id.index);
-        TextView wordA = (TextView) v.findViewById(R.id.wordA);
-        TextView wordB = (TextView) v.findViewById(R.id.wordB);
+        findView(v);
+        setViews(cursor);
 
-        index.setText(String.valueOf(cursor.getPosition() + 1));
-        wordA.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_WORD_A)));
-        wordB.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_WORD_B)));
-
+        // If id in selectedItemsList than is selected thus color gray
         if (selectedItemsList.contains(id)) {
             v.setBackgroundColor(Color.LTGRAY);
         } else {
             v.setBackgroundColor(Color.TRANSPARENT);
         }
+    }
+
+    /**
+     * Find the view
+     * @param v view
+     */
+    private void findView(View v) {
+        index = (TextView) v.findViewById(R.id.index);
+        wordA = (TextView) v.findViewById(R.id.wordA);
+        wordB = (TextView) v.findViewById(R.id.wordB);
+
+    }
+
+    /**
+     * Set the views
+     * @param cursor cursor
+     */
+    private void setViews(Cursor cursor) {
+        index.setText(String.valueOf(cursor.getPosition() + 1));
+        wordA.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_WORD_A)));
+        wordB.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.STR_WORD_B)));
+
     }
 }

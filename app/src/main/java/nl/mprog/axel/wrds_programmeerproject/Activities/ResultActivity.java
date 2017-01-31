083ff797dec5b0a6edcbd9f8c86a028028fc7641
MangoDream1 <axel.verheul@gmail.com>
2017-01-31 15:55:49 +0100

    ResultActivity finished commenting

--- app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Activities/ResultActivity.java
+++ app/src/main/java/nl/mprog/axel/wrds_programmeerproject/Activities/ResultActivity.java
@@ -19,6 +19,11 @@ import java.util.List;
 import nl.mprog.axel.wrds_programmeerproject.Database.DatabaseManager;
 import nl.mprog.axel.wrds_programmeerproject.R;
 
+/**
+ * ResultActivity shows the score of the exam and a pie chart showing the distribution of mistakes
+ * and right answer.
+ */
+
 public class ResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     DatabaseManager dbm;
@@ -32,28 +37,44 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         dbm = DatabaseManager.getInstance();
 
         Intent intent = getIntent();
-
         listId = intent.getLongExtra("id", 0L);
-        long sizeList = dbm.countListWords(listId);
-        int nMistakes = dbm.getNumberOfMistakes(listId);
 
+        setButtons();
+        setScore();
+        createPieChart();
+    }
+
+    /**
+     * Sets buttons of view
+     */
+    private void setButtons() {
         findViewById(R.id.continue_button).setOnClickListener(this);
         findViewById(R.id.retry_all_button).setOnClickListener(this);
         findViewById(R.id.retry_mistakes_button).setOnClickListener(this);
+    }
+
+    /**
+     * Sets score to TextView
+     */
+    private void setScore() {
+        long sizeList = dbm.countListWords(listId);
+        int nMistakes = dbm.getNumberOfMistakes(listId);
 
         String score = calculateScore(sizeList, nMistakes);
-
         ((TextView) findViewById(R.id.score)).setText(score);
 
         // If max score cannot retry mistakes
         if (score.equals("10")) {
             findViewById(R.id.retry_mistakes_button).setEnabled(false);
         }
-
-        createPieChart();
-
     }
 
+    /**
+     * Calculates score
+     * @param total     total number of words
+     * @param nMistakes number of mistakes
+     * @return          return String score
+     */
     private String calculateScore(long total, int nMistakes) {
         if (nMistakes > total) {
             return "1";
@@ -62,6 +83,9 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         return String.valueOf(Math.round(100 + 900 / ((double) total / (total - nMistakes)))/100);
     }
 
+    /**
+     * Creates Pie Chart
+     */
     private void createPieChart() {
         List<PieEntry> entries = new ArrayList<>();
         entries.add(new PieEntry(dbm.getNumberOfMistakes(listId), "mistakes"));
@@ -72,9 +96,12 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
                 getResources().getColor(R.color.pieChartGreen));
 
         createPieChart(dataSet);
-
     }
 
+    /**
+     * Adds data to Pie Chart and styles legend
+     * @param dataSet   pieChart data
+     */
     private void createPieChart(PieDataSet dataSet) {
         PieChart pieChart = (PieChart) findViewById(R.id.pieChart);
 
@@ -94,7 +121,10 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
         pieChart.setDrawEntryLabels(false);
     }
 
-
+    /**
+     * onClick all buttons finish
+     * @param v view
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -103,24 +133,36 @@ public class ResultActivity extends AppCompatActivity implements View.OnClickLis
                 break;
 
             case R.id.retry_all_button:
-                Intent intent = new Intent(this, ExamActivity.class);
-                intent.putExtra("id", listId);
-
-                startActivity(intent);
+                retryAllButton();
                 finish();
-
                 break;
 
             case R.id.retry_mistakes_button:
-                intent = new Intent(this, ExamActivity.class);
-                intent.putExtra("id", listId);
-                intent.putExtra("isRetryMistakes", true);
-
-                startActivity(intent);
+                retryMistakesButton();
                 finish();
-
                 break;
         }
+    }
+
+    /**
+     * Retry all button starts ExamActivity of same list
+     */
+    private void retryAllButton() {
+        Intent intent = new Intent(this, ExamActivity.class);
+        intent.putExtra("id", listId);
+
+        startActivity(intent);
+    }
+
+    /**
+     * Retry mistakes button starts ExamActivity of same list retrying only mistakes
+     */
+    private void retryMistakesButton() {
+        Intent intent = new Intent(this, ExamActivity.class);
+        intent.putExtra("id", listId);
+        intent.putExtra("isRetryMistakes", true);
+
+        startActivity(intent);
     }
 }
 
